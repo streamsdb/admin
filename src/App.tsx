3255109a5a070094ec8fac36042f146763a34e80
Pieter Joost <pj@born2code.net>
2019-06-27 09:12:15 +0200

    add layout container

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import Breadcrumbs from './Breadcrumbs';
 
 function BasicExample() {
-  return <div>
+  return <>
       <Breadcrumbs />
+    <div className="container">
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/db/:database/streams" render={( {match}: any) => (<Streams database={match.params.database} /> )} />
@@ -17,19 +18,8 @@ function BasicExample() {
         return (<Stream database={match.params.database} stream={match.params.stream} from={match.params.from === "last" ? -10 : match.params.from } limit={10} open={2} /> )}
       } />
     </Switch>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/db/default/streams">Streams</Link>
-        </li>
-      </ul>
-
-      <hr />
     </div>
-  </div>
+    </>
 }
 
 function Home() {
