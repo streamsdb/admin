d74f15c8fd6642a932c1a899e09f824e776078c5
Pieter Joost <pj@born2code.net>
2019-07-12 19:42:22 +0200

    update grahpql types

---
+++ src/db/types/Databases.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: Databases
+// ====================================================
+
+export interface Databases_databases {
+  __typename: "DatabasesPage";
+  names: string[] | null;
+}
+
+export interface Databases {
+  databases: Databases_databases;
+}
