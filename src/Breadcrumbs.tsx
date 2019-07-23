173c70fd33faf74454a400127dabffa71cfefeca
Pieter Joost <pj@born2code.net>
2019-07-23 12:53:57 +0200

    make last breadcrumb item bold

--- src/Breadcrumbs.tsx
+++ src/Breadcrumbs.tsx
@@ -8,8 +8,10 @@ import withBreadcrumbs, {
 } from "react-router-breadcrumbs-hoc";
 
 export default (routes: BreadcrumbsRoute[], excludePaths: string[]) => withBreadcrumbs(routes, { excludePaths })(({ breadcrumbs }: InjectedProps<unknown>) => {
+  const withoutHome = breadcrumbs.slice(1);
+
   const links = breadcrumbs.slice(1).map(({ breadcrumb, match }, index) => (
-    <Link key={match.url} color="inherit"  style={ index > 1 ? { fontWeight: 'normal' } : { fontWeight: 'bold' } } component={RouterLink} to={match.url}>{breadcrumb}</Link>
+    <Link key={match.url} color="inherit"  style={ index !== withoutHome.length -1 ? { fontWeight: 'normal' } : { fontWeight: 'bold' } } component={RouterLink} to={match.url}>{breadcrumb}</Link>
   ));
 
   return <Breadcrumbs color="inherit" separator="/" aria-label="Breadcrumb">
