0f7eefdffa496aa2c09203aedb0cb66eac3f41b3
Pieter Joost <pvandesande@deloitte.nl>
2019-11-14 00:28:33 +0100

    default to /api for api endpoint

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,5 +1,5 @@
 export let config = {
-  graphqlEndpoint: process.env.REACT_APP_GRAPHQL_ENDPOINT || "http://localhost:8080",
+  graphqlEndpoint: process.env.REACT_APP_GRAPHQL_ENDPOINT || "/api",
   sentryDsn: process.env.REACT_APP_SENTRY_DSN,
   gaId: process.env.REACT_APP_GA_ID
 }
