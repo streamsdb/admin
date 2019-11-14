b6cae331837c39330666ed5dce38eb82057b6ecb
Pieter Joost <pj@born2code.net>
2019-11-14 13:30:00 +0100

    add apache style request logging

--- Server.js
+++ Server.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
 const proxy = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
+
+const port = process.env.PORT || 3000;
+const api = process.env.API || 'http://localhost:5000'
+
+app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.use('/api',
   proxy({
-    target: process.env.API || 'http://localhost:5000',
-    pathRewrite: {'^/api': ''}
+    target: api,
+    pathRewrite: {'^/api': ''},
     changeOrigin: true,
   })
 );
-
-let port = process.env.PORT || 9000;
 
 console.log("listening at :" + port);
 app.listen(port);
